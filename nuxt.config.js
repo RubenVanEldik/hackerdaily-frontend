1f94bfe3256b3d388c05c02438b90e9edc90d575
Ruben <Ruben@MBP-van-Ruben-2.domain_not_set.invalid>
2020-07-10 18:30:11 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,30 @@
+
+export default {
+  mode: 'universal',
+  target: 'server',
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [
+  ],
+  plugins: [
+  ],
+  components: true,
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+  build: {
+  }
+}
