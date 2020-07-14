ef8a6f27ac4d03387196bdf8b2c3764eb5a73bfb
Ruben <Ruben@MBP-van-Ruben-2.domain_not_set.invalid>
2020-07-15 00:12:32 +0200

    feat: Add the error layout

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center pt-48 sm:pt-56 md:64">
+    <div class="text-4xl">
+      {{ error.statusCode }}
+    </div>
+    <div class="text-lg">
+      {{ error.message }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
