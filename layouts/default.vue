87f7ed09e830cc3efe62bb96d2a949f5755c0a0e
Ruben <Ruben@MBP-van-Ruben-2.domain_not_set.invalid>
2020-07-16 15:18:25 +0200

    fix: Make the backgroundcolor gray

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-orange-100 min-h-screen flex justify-center font-serif">
+  <div class="bg-gray-100 min-h-screen flex justify-center font-serif">
     <div class="w-screen sm:w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 max-w-2xl px-3 sm:px-0">
       <app-bar />
       <nuxt class="pb-5" />
