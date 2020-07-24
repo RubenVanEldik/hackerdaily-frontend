91b430223f652394619873196ee926890110e8f3
Ruben <Ruben@MBP-van-Ruben-2.localdomain>
2020-07-24 12:44:08 +0200

    fix: Add the custom domain option to the Simple Analytics plugin

--- plugins/simpleAnalytics.js
+++ plugins/simpleAnalytics.js
@@ -1,4 +1,7 @@
 import SimpleAnalytics from 'simple-analytics-vue'
 import Vue from 'vue'
 
-Vue.use(SimpleAnalytics, { skip: process.env.NODE_ENV !== 'production' })
+Vue.use(SimpleAnalytics, {
+  skip: process.env.NODE_ENV !== 'production',
+  domain: process.env.SIMPLE_ANALYTICS_URL
+})
