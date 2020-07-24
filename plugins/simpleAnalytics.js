7408654864fcf8ddc95131f172dfc1503b0a835d
Ruben <Ruben@MBP-van-Ruben-2.localdomain>
2020-07-24 23:47:08 +0200

    fix: Use the runtime configs in the Simple Analytics plugin

--- plugins/simpleAnalytics.js
+++ plugins/simpleAnalytics.js
@@ -1,7 +1,9 @@
 import SimpleAnalytics from 'simple-analytics-vue'
 import Vue from 'vue'
 
-Vue.use(SimpleAnalytics, {
-  skip: process.env.NODE_ENV !== 'production',
-  domain: process.env.SIMPLE_ANALYTICS_URL
-})
+export default (context) => {
+  Vue.use(SimpleAnalytics, {
+    skip: context.$config.NODE_ENV !== 'production',
+    domain: context.$config.SIMPLE_ANALYTICS_URL
+  })
+}
