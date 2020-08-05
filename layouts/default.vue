3316fd3b61a058ed4faff1a67d989fe729d8c8fd
Ruben <>
2020-08-05 09:25:51 +0200

    refactor: Simplify the default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,14 @@
 <template>
-  <div class="min-h-screen flex justify-center">
-    <div class="w-screen min-h-full flex flex-col sm:w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 max-w-2xl">
-      <app-bar class="px-3 sm:px-0" />
-      <main class="flex-grow mb-6 px-3 sm:px-0">
-        <nuxt />
-      </main>
-      <footer class="mb-4 text-sm text-center text-gray-600 italic">
-        <a href="mailto:contact@hackerdaily.io">
-          Made with ♥ in Europe
-        </a>
-      </footer>
-    </div>
+  <div class="min-h-screen w-screen m-auto flex flex-col sm:w-11/12 md:w-4/5 lg:w-3/4 xl:w-2/3 max-w-2xl">
+    <app-bar class="px-3 sm:px-0" />
+    <main class="flex-grow mb-6 px-3 sm:px-0">
+      <nuxt />
+    </main>
+    <footer class="mb-4 text-sm text-center text-gray-600 italic">
+      <a href="mailto:contact@hackerdaily.io">
+        Made with ♥ in Europe
+      </a>
+    </footer>
   </div>
 </template>
 
