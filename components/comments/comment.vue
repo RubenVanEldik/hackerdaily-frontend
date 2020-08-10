d9d7d459b8c65c9b93f0d7bba018df75ad58f4a3
Ruben <>
2020-08-10 14:53:59 +0200

    refactor: Move all comment parts to a single component

--- components/comments/comment.vue
+++ components/comments/comment.vue
@@ -1,22 +1,55 @@
 <template>
-  <div
-    class="prose break-words my-1"
-    style="max-width: 100%"
-    v-html="parsedText"
-  />
+  <div class="my-5">
+    <div class="text-gray-700 font-semibold">
+      <span
+        class="inline-block w-4 text-center cursor-pointer"
+        @click="() => showComment = !showComment"
+        v-text="showComment ? '−' : '+'"
+      />
+      <span v-text="header" />
+    </div>
+    <div
+      v-if="showComment && comment.text"
+      class="prose break-words my-1"
+      style="max-width: 100%"
+      v-html="parsedText"
+    />
+    <div v-if="showComment">
+      <comment
+        v-for="childComment in comment.comments"
+        :key="childComment.id"
+        :comment="childComment"
+        class="pl-5 sm:pl-6"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+dayjs.extend(relativeTime)
+
 export default {
+  name: 'Comment',
   props: {
-    text: {
-      type: String,
+    comment: {
+      type: Object,
       required: true
     }
   },
+  data () {
+    return {
+      showComment: true
+    }
+  },
   computed: {
+    header () {
+      return `${this.comment.user_id || '[deleted]'} · ${dayjs(this.comment.posted_at).fromNow()}`
+    },
     parsedText () {
-      const parts = this.text.split('<p>')
+      const parts = this.comment.text.split('<p>')
       return parts
         .map((part, index) => {
           // Check if the paragraph is a blockquote
