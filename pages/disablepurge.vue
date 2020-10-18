45d1c1a41e5e81f19d12d0d0c8558eee60d4be5e
Ruben <>
2020-10-18 18:01:00 +0200

    fix: Add a page with all styles so purgecss won't purge them when they're only used in articles

---
+++ pages/disablepurge.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="prose dark:prose-dark">
+    <a href="#">A</a>
+    <strong>Bold</strong>
+    <ol>
+      <li>Li 1</li>
+      <li>Li 2</li>
+    </ol>
+    <ul>
+      <li>Li 1</li>
+      <li>Li 2</li>
+    </ul>
+    <hr>
+    <blockquote>Blockquote</blockquote>
+    <figure>
+      <figcaption>Figcaptionf</figcaption>
+    </figure>
+    <h1>H1</h1>
+    <h2>H2</h2>
+    <h3>H3</h3>
+    <h4>H4</h4>
+    <h5>H5</h5>
+    <h6>H6</h6>
+    <em>Em</em>
+    <code>Code</code>
+    <pre>Pre</pre>
+    <table>
+      <thead>Thead</thead>
+      <tbody>
+        <tr>Tr</tr>
+      </tbody>
+    </table>
+  </div>
+</template>
