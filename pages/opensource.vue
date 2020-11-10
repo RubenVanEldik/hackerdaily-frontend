6778a1d3556e6208d5c452721a1034bc12f7578a
Ruben <ruben@hackerdaily.io>
2020-11-10 14:37:54 +0100

    fix: Improve the copy on the open source page

--- pages/opensource.vue
+++ pages/opensource.vue
@@ -2,28 +2,29 @@
   <div class="prose dark:prose-dark m-auto">
     <h2>Open source</h2>
     <p>
-      Some users want to contribute to HackerDaily and a few even wanted to make their own version. Therefore, I decided to make everything open source.
+      Multiple early users asked how to contribute to HackerDaily and a few even wanted to build their own version. Therefore, I decided to make everything open source.
     </p>
     <p>
-      This page contains a basic overview of the technologies that are used and links to the repositories. This is the first project that I open source, so <a href="mailto:contact@hackerdaily.io">please let me know</a> what can be improved from an OS point of view.
+      This page contains a basic overview of the technologies that are used and links to the repositories. This is the first project that I will open source, so <a href="mailto:contact@hackerdaily.io">please let me know</a> what can be improved from an OS point of view.
     </p>
+
     <strong>More extensive README's will be published in the coming days.</strong>
 
     <h3>
-      <a href="https://github.com/RubenVanEldik/hackerdaily-frontend">Front end</a>
+      <a href="https://github.com/RubenVanEldik/hackerdaily-frontend">Frontend</a>
     </h3>
     <p>
-      The front-end is created with <a href="https://nuxtjs.org/">Nuxt.js</a> and hosted on Netlify. It uses <a href="https://apollo.vuejs.org/">Apollo</a> to fetch and cache all data and <a href="https://tailwindcss.com/">Tailwind CSS</a> to style the pages. <a href="https://simpleanalytics.com/">Simple Analytics</a> is used for privacy friendly analytics.
+      The frontend is created with <a href="https://nuxtjs.org/">Nuxt.js</a> and hosted on Netlify. It uses <a href="https://apollo.vuejs.org/">Apollo</a> to fetch and cache all data and <a href="https://tailwindcss.com/">Tailwind CSS</a> to style the pages. <a href="https://referral.simpleanalytics.com/hackerdaily">Simple Analytics</a> is used for privacy friendly analytics.
     </p>
     <p>
       The analytics can be viewed <a href="https://simpleanalytics.com/hackerdaily.io">here</a>.
     </p>
 
     <h3>
-      <a href="#">Back end</a>
+      <a href="#">Backend (coming soon)</a>
     </h3>
     <p>
-      The back end is a basic <a href="https://hasura.io/">Hasura</a> engine running on Heroku. It's so basic that I have not yet version controlled it. As soon as that's done that the code will be open sourced.
+      The backend is a basic <a href="https://hasura.io/">Hasura</a> engine running on Heroku. It's so basic that I have not yet version controlled it. As soon as that's done that the code will be open sourced.
     </p>
 
     <h3>
