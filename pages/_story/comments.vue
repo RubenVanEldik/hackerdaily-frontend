7621e73bf4b5b80b40bdfa5f02b0d8a0815dbb97
Ruben van Eldik <rubenvaneldik@protonmail.com>
2021-11-30 17:23:33 +0100

    feat: Show the cached story data when the comments are being fetched

--- pages/_story/comments.vue
+++ pages/_story/comments.vue
@@ -1,13 +1,18 @@
 <template>
-  <fallback
-    :is-loading="$apollo.loading"
-    :is-empty="!story"
-    loading-message="Loading comments..."
-    empty-message="The comments for this story are not saved."
-  >
-    <comments-header :story="story" />
-    <comments-all :story="story" />
-  </fallback>
+  <div>
+    <comments-header
+      v-if="story || storyPreview"
+      :story="story || storyPreview"
+    />
+    <fallback
+      :is-loading="$apollo.loading"
+      :is-empty="!story"
+      loading-message="Loading comments..."
+      empty-message="The comments for this story are not saved."
+    >
+      <comments-all :story="story" />
+    </fallback>
+  </div>
 </template>
 <script>
 import commentsQuery from '~/apollo/fetchComments.gql'
@@ -16,9 +21,10 @@ export default {
   validate ({ params }) {
     return /^\d+$/.test(params.story)
   },
-  head () {
-    return {
-      title: this.story?.title
+  computed: {
+    storyPreview () {
+      // This is a ridiculously ugly solution, please let me know if there's a better way! (https://github.com/vuejs/vue-apollo/discussions/1075)
+      return this.$apolloProvider.defaultClient.cache.data.data[`stories:${this.$route.params.story}`]
     }
   },
   apollo: {
@@ -29,6 +35,11 @@ export default {
           id: this.$route.params.story
         }
       }
+    }
+  },
+  head () {
+    return {
+      title: this.story?.title
     }
   }
 }
