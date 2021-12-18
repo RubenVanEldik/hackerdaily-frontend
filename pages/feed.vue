bab762e04db22881690823ac9aad631cf6e3cf6e
Ruben van Eldik <rubenvaneldik@protonmail.com>
2021-12-18 11:34:38 +0100

    fix: Improve the /feed page copy

--- pages/feed.vue
+++ pages/feed.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="prose dark:prose-dark m-auto">
-    <h2>We've got an RSS feed!</h2>
+    <h2>We've got RSS!</h2>
     <p>
-      After many requests we've finally created an RSS feed!
+      After many requests we have finally created an RSS feed!
     </p>
     <p>
       Because what 'yesterday' is depends on where you're are, we've created an RSS feed for each time zone. You can select your preferred timezone below and you'll be redirected to the correct feed.
