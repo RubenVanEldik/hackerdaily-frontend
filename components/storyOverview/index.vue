9c19b60ee7051865c1cf31bbb31135e95bb5acd1
Ruben van Eldik <rubenvaneldik@protonmail.com>
2023-01-03 01:07:47 +0100

    feat: Show a status message about the upcoming migration

--- components/storyOverview/index.vue
+++ components/storyOverview/index.vue
@@ -5,6 +5,23 @@
     loading-message="Loading stories..."
     empty-message="There are no stories saved for this day."
   >
+    <div
+      v-if="showMessage"
+      class="flex w-full mt-1 mb-5 px-5 py-3 rounded-lg bg-gray-200 dark:bg-gray-800 bg-opacity-75 dark:bg-opacity-75"
+    >
+      <div
+        class="text-gray-700 dark:text-gray-300"
+        v-text="message"
+      />
+      <div class="-mt-4 -mr-6">
+        <button
+          class="block h-6 w-6 rounded-full bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-gray-200 leading-none"
+          @click="hideMessage"
+        >
+          ×
+        </button>
+      </div>
+    </div>
     <story-overview-item
       v-for="(story, index) in stories"
       :key="story.id"
@@ -37,6 +54,29 @@ export default {
           endDate: this.endDate
         }
       }
+    }
+  },
+  data () {
+    return {
+      message: 'I will be migrating the backend and database from Heroku to Render on Tuesday. This could cause some temporary disruptions. My apologies in advance!'
+    }
+  },
+  computed: {
+    showMessage () {
+      if (!this.message) {
+        return false
+      }
+
+      // Only show the message if the hide_message key in local storage is not the same as the current message
+      return localStorage.getItem('hide_message') !== this.message
+    }
+  },
+  methods: {
+    hideMessage () {
+      // Set the hide_message key in local storage to the current message
+      localStorage.setItem('hide_message', this.message)
+      // Remove the message for now
+      this.message = null
     }
   }
 }
